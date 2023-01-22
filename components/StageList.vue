2d6501aff9ba060c90ccf8f1c46a2a2dbec4d105
ezpzlmnsqz1337 <mazlixek@seznam.cz>
2023-01-22 11:49:49 +0100

    chore(TrackPanel): extract duplicated code to separate components

---
+++ components/StageList.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="__wrapper">
+    <div class="__heading">
+      {{ heading }}
+    </div>
+    <div
+      v-for="s in stages"
+      :key="`stage-${s.id}`"
+      class="__item stage"
+      :class="{__active: isActiveStage(s)}"
+      @click="setActiveStage(s)"
+    >
+      <div>
+        <div>{{ s.name }}</div> <div>{{ s.lengthKm }}km</div>
+      </div>
+      <div v-if="getTimesForStage(s).length > 0">
+        {{ getTimesForStage(s).length }} time(s)
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations, mapGetters } from 'vuex'
+
+export default {
+  name: 'StageList',
+  props: {
+    heading: {
+      type: String,
+      default: ''
+    },
+    stages: {
+      type: Array,
+      default: () => []
+    }
+  },
+  computed: {
+    ...mapState(['activeStage']),
+    ...mapGetters(['getTimesForStage'])
+  },
+  methods: {
+    ...mapMutations(['setActiveStage']),
+    isActiveStage (stage) {
+      if (!stage || !this.activeStage) { return false }
+      return stage.id === this.activeStage.id
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.__stage {
+  text-align: center;
+  padding: 0 0.5rem;
+}
+
+.__heading {
+  text-align: center;
+  margin-bottom: 1rem;
+}
+
+.__item {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 0.8rem 1rem;
+  margin-bottom: 0.5rem;
+  font-size: 1rem;
+  border-radius: 0.3rem;
+  background-color: #777777;
+
+  &:hover {
+    cursor: pointer;
+    background-color: #0045e0;
+  }
+
+  &.__active {
+    background-color: #0045E0;
+  }
+}
+</style>
--- components/TrackPanel.vue
+++ components/TrackPanel.vue
@@ -4,15 +4,7 @@
       {{ activeLocation ? 'Stages' : 'Locations' }}
     </h2>
     <div v-if="!activeLocation" class="__locations">
-      <div
-        v-for="(l, index) in locations"
-        :key="`location-${index}`"
-        class="__item __location"
-        :class="{__active: isActiveLocation(l)}"
-        @click="setActiveLocation(l)"
-      >
-        <div>{{ l.name }}</div> <div><span :class="`fp fp-lg ${l.countryCode}`" /></div>
-      </div>
+      <LocationList :locations="locations" />
     </div>
     <div v-if="activeLocation" class="__stages">
       <div class="__back">
@@ -24,63 +16,22 @@
         </div>
         <div><span :class="`fp fp-lg ${activeLocation.countryCode}`" /></div>
       </div>
-      <div class="__heading">
-        Forward
-      </div>
-      <div
-        v-for="(s, index) in activeLocation.forward"
-        :key="`stage-forward-${index}`"
-        class="__item stage"
-        :class="{__active: isActiveStage(s)}"
-        @click="setActiveStage(s)"
-      >
-        <div>
-          <div>{{ s.name }}</div> <div>{{ s.lengthKm }}km</div>
-        </div>
-        <div v-if="getTimesForStage(s).length > 0">
-          {{ getTimesForStage(s).length }} time(s)
-        </div>
-      </div>
-      <div class="__heading">
-        Reverse
-      </div>
-      <div
-        v-for="(s, index) in activeLocation.reverse"
-        :key="`stage-reverse-${index}`"
-        class="__item stage"
-        :class="{__active: isActiveStage(s)}"
-        @click="setActiveStage(s)"
-      >
-        <div>
-          <div>{{ s.name }}</div> <div>{{ s.lengthKm }}km</div>
-        </div>
-        <div v-if="getTimesForStage(s).length > 0">
-          {{ getTimesForStage(s).length }} time(s)
-        </div>
-      </div>
+      <StageList heading="Forward" :stages="activeLocation.forward" />
+      <StageList heading="Reverse" :stages="activeLocation.reverse" />
     </div>
   </div>
 </template>
 
 <script>
-import { mapState, mapMutations, mapGetters } from 'vuex'
+import { mapState, mapMutations } from 'vuex'
 
 export default {
   name: 'TrackPanel',
   computed: {
-    ...mapState(['locations', 'stages', 'activeLocation', 'activeStage', 'stagesShow', 'locationsShow']),
-    ...mapGetters(['getTimesForStage'])
+    ...mapState(['locations', 'activeLocation'])
   },
   methods: {
-    ...mapMutations(['setActiveStage', 'setActiveLocation', 'showStages', 'showLocations', 'showRightPanel']),
-    isActiveLocation (location) {
-      if (!location || !this.activeLocation) { return false }
-      return location.id === this.activeLocation.id
-    },
-    isActiveStage (stage) {
-      if (!stage || !this.activeStage) { return false }
-      return stage.id === this.activeStage.id
-    },
+    ...mapMutations(['setActiveLocation']),
     back () {
       this.setActiveLocation(null)
     }
@@ -112,54 +63,6 @@ export default {
 .__heading {
   text-align: center;
   margin-bottom: 1rem;
-}
-
-.__btn {
-  display: inline-block;
-  font-size: 1rem;
-  color: white;
-  background-color: #a4a4a5;
-  padding: 0.5rem 1rem;
-  border-radius: 0.3rem;
-
-  &:hover {
-    background-color: #0045e0;
-    cursor: pointer;
-  }
-
-  &.__active {
-    background-color: #0045e0;
-  }
-}
-
-h2 {
-  margin-bottom: 1rem;
-}
-
-.__item {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  padding: 0.8rem 1rem;
-  margin-bottom: 0.5rem;
-  font-size: 1rem;
-  border-radius: 0.3rem;
-  background-color: #777777;
-
-  &:hover {
-    cursor: pointer;
-    background-color: #0045e0;
-  }
-
-  &.__active {
-    background-color: #0045E0;
-  }
-}
-
-.__location {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
 }
 
 .__back {
